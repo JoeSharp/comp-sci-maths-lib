320fdd463cde0d9e7c7087410b921986ada2c4b7
Mr Sharp <jms@marling.school>
2021-04-19 23:05:02 +0100

    sort of got 4way dmux working, but there is something wrong

---
+++ src/computation/nand/Dmux4Way.test.ts
@@ -0,0 +1,87 @@
+// | in  | sel  |  a  |  b  |  c  |  d  |
+// |  0  |  00  |  0  |  0  |  0  |  0  |
+// |  0  |  01  |  0  |  0  |  0  |  0  |
+// |  0  |  10  |  0  |  0  |  0  |  0  |
+// |  0  |  11  |  0  |  0  |  0  |  0  |
+// |  1  |  00  |  1  |  0  |  0  |  0  |
+// |  1  |  01  |  0  |  1  |  0  |  0  |
+// |  1  |  10  |  0  |  0  |  1  |  0  |
+// |  1  |  11  |  0  |  0  |  0  |  1  |
+
+import Dmux4Way from "./Dmux4Way";
+import { booleanToBinArray, boolToBin } from "./types";
+
+interface TestCase {
+    input: boolean;
+    sel: boolean[];
+    a: boolean;
+    b: boolean;
+    c: boolean;
+    d: boolean;
+}
+
+const TEST_CASES: TestCase[] = [
+    {
+        input: false,
+        sel: [false, false],
+        a: false, b: false, c: false, d: false,
+    },
+    {
+        input: false,
+        sel: [false, true],
+        a: false, b: false, c: false, d: false,
+    },
+    {
+        input: false,
+        sel: [true, false],
+        a: false, b: false, c: false, d: false,
+    },
+    {
+        input: false,
+        sel: [true, true],
+        a: false, b: false, c: false, d: false,
+    },
+    {
+        input: true,
+        sel: [false, false],
+        a: true, b: false, c: false, d: false,
+    },
+    {
+        input: true,
+        sel: [false, true],
+        a: false, b: false, c: true, d: false,
+    },
+    {
+        input: true,
+        sel: [true, false],
+        a: false, b: true, c: false, d: false,
+    }, // JOE TODO SOMETHING WRONG HERE
+    {
+        input: true,
+        sel: [true, true],
+        a: false, b: false, c: false, d: true,
+    },
+]
+
+describe('Dmux 4 Way', () => {
+    const aReceiver = jest.fn();
+    const bReceiver = jest.fn();
+    const cReceiver = jest.fn();
+    const dReceiver = jest.fn();
+    const demux = new Dmux4Way();
+    demux.connectA(aReceiver);
+    demux.connectB(bReceiver);
+    demux.connectC(cReceiver);
+    demux.connectD(dReceiver);
+
+    TEST_CASES.forEach(({ input, sel, a, b, c, d }) => {
+        test(`In: ${boolToBin(input)}, Sel: ${booleanToBinArray(sel)}, a: ${boolToBin(a)}, b: ${boolToBin(b)}, c: ${boolToBin(c)}, d: ${boolToBin(d)}`, () => {
+            demux.sendInput(input);
+            demux.sendSel(sel);
+            expect(aReceiver).toHaveBeenLastCalledWith(a);
+            expect(bReceiver).toHaveBeenLastCalledWith(b);
+            expect(cReceiver).toHaveBeenLastCalledWith(c);
+            expect(dReceiver).toHaveBeenLastCalledWith(d);
+        })
+    })
+})
---
+++ src/computation/nand/Dmux4Way.ts
@@ -0,0 +1,80 @@
+import { Consumer } from "../../types";
+import And from "./And";
+import Dmux from "./Dmux";
+import Not from "./Not";
+
+/**
+ * 4-way demultiplexor:
+ * {a, b, c, d} = {in, 0, 0, 0} if sel == 00
+ *                {0, in, 0, 0} if sel == 01
+ *                {0, 0, in, 0} if sel == 10
+ *                {0, 0, 0, in} if sel == 11
+ */
+//  CHIP DMux4Way {
+//     IN in, sel[2];
+//     OUT a, b, c, d;
+
+//     PARTS:
+//     Not(in=sel[1], out=notSel1);
+//     And(a=in, b=notSel1, out=inAndNotSel1);
+//     DMux(in=inAndNotSel1, sel=sel[0], a=a, b=b);
+
+//     And(a=in, b=sel[1], out=inAndSel1);
+//     DMux(in=inAndSel1, sel=sel[0], a=c, b=d);    
+// }
+class Dmux4Way {
+    notSel1: Not;
+    inAndNotSel1: And;
+    dmuxAB: Dmux;
+
+    inAndSel1: And;
+    dmuxCD: Dmux;
+
+    constructor() {
+        this.notSel1 = new Not();
+        this.inAndNotSel1 = new And();
+        this.dmuxAB = new Dmux();
+        this.inAndSel1 = new And();
+        this.dmuxCD = new Dmux();
+    
+        this.notSel1.connectOutput(this.inAndNotSel1.connectB());
+        this.inAndNotSel1.connectOutput(this.dmuxAB.connectInput());
+        this.inAndSel1.connectOutput(this.dmuxCD.connectInput());
+    }
+
+    sendInput(input: boolean) {
+        this.inAndNotSel1.sendA(input);
+        this.inAndSel1.sendA(input);
+    }
+
+    sendSel(sel: boolean[], startIndex: number = 0) {
+        sel.forEach((s, i) => {
+            const index = startIndex + i;
+            switch(index) {
+                case 0:
+                    this.dmuxAB.sendSel(s);
+                    this.dmuxCD.sendSel(s);
+                    break;
+                case 1:
+                    this.notSel1.sendIn(s);
+                    this.inAndSel1.sendB(s);
+                    break;
+            }
+        })
+    }
+
+    connectA(receiver: Consumer<boolean>) {
+        this.dmuxAB.connectA(receiver);
+    }
+    connectB(receiver: Consumer<boolean>) {
+        this.dmuxAB.connectB(receiver);
+    }
+    connectC(receiver: Consumer<boolean>) {
+        this.dmuxCD.connectA(receiver);
+    }
+    connectD(receiver: Consumer<boolean>) {
+        this.dmuxCD.connectB(receiver);
+    }
+}
+
+export default Dmux4Way;
