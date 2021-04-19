bd5170259329cd790101209c3a9e26e30c039a00
Mr Sharp <jms@marling.school>
2021-04-19 22:22:41 +0100

    mux16 and16 implemented

---
+++ src/computation/nand/And16.test.ts
@@ -0,0 +1,58 @@
+import { Consumer } from "../../types";
+import And16 from "./And16";
+import { booleanToBinArray, binaryToBoolArray } from "./types";
+
+interface TestCase {
+    a: boolean[];
+    b: boolean[];
+    expected: boolean[];
+}
+
+const TEST_CASES: TestCase[] = [
+    {
+        a: binaryToBoolArray('0000000000000000'),
+        b: binaryToBoolArray('0000000000000000'),
+        expected: binaryToBoolArray('0000000000000000')
+    },
+    {
+        a: binaryToBoolArray('0000000000000000'),
+        b: binaryToBoolArray('1111111111111111'),
+        expected: binaryToBoolArray('0000000000000000')
+    },
+    {
+        a: binaryToBoolArray('1111111111111111'),
+        b: binaryToBoolArray('1111111111111111'),
+        expected: binaryToBoolArray('1111111111111111')
+    },
+    {
+        a: binaryToBoolArray('1010101010101010'),
+        b: binaryToBoolArray('0101010101010101'),
+        expected: binaryToBoolArray('0000000000000000')
+    },
+    {
+        a: binaryToBoolArray('0011110011000011'),
+        b: binaryToBoolArray('0000111111110000'),
+        expected: binaryToBoolArray('0000110011000000')
+    },
+    {
+        a: binaryToBoolArray('0001001000110100'),
+        b: binaryToBoolArray('1001100001110110'),
+        expected: binaryToBoolArray('0001000000110100')
+    }
+]
+
+
+describe('AND 16', () => {
+    const and16 = new And16();
+    const receivers: Consumer<boolean>[] = Array(16).fill(null).map(() => jest.fn());
+    and16.connectOutput(receivers);
+
+    TEST_CASES.forEach(({ a, b, expected }) => {
+        test(`${booleanToBinArray(a)} AND ${booleanToBinArray(b)} = ${booleanToBinArray(expected)}`, () => {
+            and16.sendA(a);
+            and16.sendB(b);
+
+            expected.forEach((e, i) => expect(receivers[i]).toHaveBeenLastCalledWith(e));
+        })
+    })
+})
--- src/computation/nand/types.ts
+++ src/computation/nand/types.ts
@@ -3,3 +3,10 @@ export interface TwoInOneOutTestCase {
     b: boolean;
     expected: boolean;
 }
+
+export const WORD_LENGTH = 16;
+
+export const binaryToBoolArray = (input: string): boolean[] => input.split('').map(binToBool);
+export const booleanToBinArray = (input: boolean[]): string => input.map(boolToBin).join('');
+export const boolToBin = (v: boolean) => v ? '1' : '0';
+export const binToBool = (v: string) => v === '1';
