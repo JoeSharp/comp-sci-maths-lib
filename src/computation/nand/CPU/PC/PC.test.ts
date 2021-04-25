17273411cdcdce76358423775c124d6f296f98f2
Joe Sharp <ratracejoe@hotmail.com>
2021-04-25 21:02:10 +0100

    PC working, tested to a small extent

--- src/computation/nand/CPU/PC/PC.test.ts
+++ src/computation/nand/CPU/PC/PC.test.ts
@@ -1,14 +1,20 @@
 import { binaryToNumber } from "../../../../dataRepresentation/numberBases/simpleBinary";
 import BinaryBus from "../../BinaryBus";
 import { Clock } from "../../Clocked";
-import { generateRandomWord, PIN_OUTPUT } from "../../types";
-import PC, { PIN_INCREMENT } from "./PC";
+import {
+  generateRandomWord,
+  PIN_INPUT,
+  PIN_LOAD,
+  PIN_OUTPUT,
+} from "../../types";
+import PC, { PIN_INCREMENT, PIN_RESET } from "./PC";
 
 describe("Program Counter", () => {
   test("Simple", () => {
     const clock = new Clock();
     const pc = new PC(clock);
     const jumpAddress = generateRandomWord();
+    const jumpAddressNum = binaryToNumber(jumpAddress);
 
     const sink = new BinaryBus();
     pc.getBus(PIN_OUTPUT).connect(sink);
@@ -22,5 +28,33 @@ describe("Program Counter", () => {
 
     clock.ticktock();
     expect(pcValue()).toBe(2);
+
+    clock.ticktock();
+    expect(pcValue()).toBe(3);
+
+    // Jump and count up a bit
+    pc.getBus(PIN_INPUT).send(jumpAddress);
+    pc.getPin(PIN_LOAD).send(true);
+    clock.ticktock();
+    expect(pcValue()).toBe(jumpAddressNum);
+
+    clock.ticktock();
+    expect(pcValue()).toBe(jumpAddressNum);
+
+    pc.getPin(PIN_LOAD).send(false);
+    clock.ticktock();
+    expect(pcValue()).toBe(jumpAddressNum + 1);
+    clock.ticktock();
+    expect(pcValue()).toBe(jumpAddressNum + 2);
+
+    // Reset and count up a bit
+    pc.getPin(PIN_RESET).send(true);
+    clock.ticktock();
+    expect(pcValue()).toBe(0);
+    clock.ticktock();
+    expect(pcValue()).toBe(0);
+    pc.getPin(PIN_RESET).send(false);
+    clock.ticktock();
+    expect(pcValue()).toBe(1);
   });
 });
