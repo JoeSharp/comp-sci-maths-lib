5787bd7891496aa5b4dcfe3f9188df8a22efc6db
Joe Sharp <ratracejoe@hotmail.com>
2021-05-25 15:02:11 +0100

    removing boolean logic stuff that has since been replaced

--- src/computation/booleanLogic/error/IncorrectBusError.ts
+++
@@ -1,9 +0,0 @@
-export default class IncorrectBusError extends Error {
-    chipName: string;
-    busNames: string[];
-    constructor(chipName: string, busNames: string[]) {
-        super(`Busses Incorrect for ${chipName} - ${busNames}`)
-        this.chipName = chipName;
-        this.busNames = busNames;
-    }
-}
--- src/computation/booleanLogic/error/MissingInputError.ts
+++
@@ -1,8 +0,0 @@
-export default class MissingInputError extends Error {
-    chipName: string;
-    inputNames: string[];
-    constructor(chipName: string, inputNames: string[]) {
-        super(`Missing Inputs for ${chipName} - ${inputNames}`)
-        this.inputNames = inputNames;
-    }
-}
